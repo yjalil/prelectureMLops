f7a1741f3ddd13cda118cc6fd26ae01d1aaa1c7a
sana sebban <ssebban@gmail.com>
2024-09-19 21:16:37 +0100

    passenger scaling

---
+++ project/feature_engineering/passenger_scaler.py
@@ -0,0 +1,6 @@
+from sklearn.preprocessing import MinMaxScaler
+
+def passenger_scale(df):
+    scaler = MinMaxScaler()
+    df['passenger_count_scaled'] = scaler.fit_transform(df[['passenger_count']])
+    return df

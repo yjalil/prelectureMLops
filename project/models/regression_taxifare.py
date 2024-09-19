f0814bd59f14cf67d396c8342d6bab8ad4d0b4ae
Drissialami <sanaadrissialami@gmail.com>
2024-09-19 21:30:41 +0100

    done new column night 1

--- project/models/regression_taxifare.py
+++ project/models/regression_taxifare.py
@@ -9,6 +9,16 @@ def get_secret() -> str:
     """This function returns a secret"""
     return conf.secret
 
+def categorize_time(row):
+    hour = row['pickup_datetime'].hour
+    # Check if the hour is between 20 (8 PM) and 7 (7 AM)
+    if hour >= 20 or hour < 7:
+        return 1  # Night
+    else:
+        return 0  # Not Night
+
+#df['is_night'] = df.apply(categorize_time, axis=1)
+
 
 if __name__ == "__main__":
     print(hello())

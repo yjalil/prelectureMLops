993ee8a58bba73a817c18cb5a9b8517840871f13
sana sebban <ssebban@gmail.com>
2024-09-21 16:02:02 +0100

    third  commit

--- project/models/RNN.py
+++ project/models/RNN.py
@@ -1,10 +1,11 @@
 
-from tensorflow.keras import models, layers
+from tensorflow.keras import models, layers, Sequential
 
 def initiate_compile_model():
     model = models.Sequential()
     model.add(layers.Dense(10, activation='relu', input_dim=3)) # highly recommend this option
-    model.add(layers.Dense(7, activation='relu'))                               # instead of input_dim = 13
+    model.add(layers.Dense(10, activation='relu'))
+    #model.add(layers.Dense(7, activation='tanh'))# instead of input_dim = 13
     model.add(layers.Dense(1, activation='linear'))
     model.compile(
     loss = 'mse',
